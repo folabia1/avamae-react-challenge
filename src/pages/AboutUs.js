20d0525ecf374a6ca537cc858e6c6f8cf2f4cb3c
folabia1 <27552121+folabia1@users.noreply.github.com>
2022-05-01 22:07:34 +0100

    update About Us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { LoremIpsum } from "react-lorem-ipsum";
+import roomImage2 from "../media/shutterstock_696636415.jpg";
 
 export function AboutUs() {
-  return <h1>About Us</h1>
+  return (
+    <div className="AboutUs">
+      <h1>About Us</h1>
+      <strong><LoremIpsum avgSentencesPerParagraph={2} startWithLoremIpsum={false}/></strong>
+      <p>Lorem ipsum dolor sit atem, consectetur adipiscing elit. Nunc pulvinar enim sed quam efficitur finibus. Fusce efficitur condimentum orci in hendrerit. Etiam aliquam vitae ante et scelerisque. Pellentesque commodo felis metus, nec congue nisi facilisis quis. Aenean maximus bibendum congue. Nulla pretuim elit non facilisis imperdiet. Curabitur auctor lacus turpis, quis fringilla quam faucibus sed. Sed consequat magna enim, eu efficitur purus viverra sit amet. <a href="#">Praesent varius porta blandit</a> mollis, felis ut convallis convallis.</p>
+      <LoremIpsum startWithLoremIpsum={false}/>
+      <img src={roomImage2} alt=""/>
+      <LoremIpsum startWithLoremIpsum={false}/>
+      <h2>Taria duo ut vis semper abhorreant:</h2>
+      <ul>
+        <li>Te pri efficiendi assueverit, id molestie suavitate per</li>
+        <li>Te nam dolerm rationibus repudiandae, ne ius falli aliquip consetetur</li>
+        <li>Ut qui dicant copiosae interpretaris</li>
+        <li>Ut indoctum patrioque voluptaria duo, ut vis emper abhorreant</li>
+      </ul>
+      <LoremIpsum startWithLoremIpsum={false}/>
+      <LoremIpsum startWithLoremIpsum={false}/>
+    </div> 
+  )
 }
