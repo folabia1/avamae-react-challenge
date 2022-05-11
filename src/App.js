804d645cb30bd025d13f870945a34f6777851901
folabia1 <27552121+folabia1@users.noreply.github.com>
2022-05-11 10:51:13 +0100

    update css media queries
    change from using "react-responsive" media queries library to standard css media queries

--- src/App.js
+++ src/App.js
@@ -1,71 +1,55 @@
 import "./css/App.css";
 
-import React, { createContext } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   NavLink,
 } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
 import { Home } from "./pages/Home";
 import { AboutUs } from "./pages/AboutUs";
 import { ContactUs } from "./pages/ContactUs";
 
 import companyLogo from "./media/Logo.svg";
 
-const ScreenSizeContext = createContext(null);
-
 function App() {
-  const isLargeScreen = useMediaQuery({ query: "(min-width:900px)" });
-  const isSmallScreen = useMediaQuery({ query: "(max-width: 600px)" });
-  const isMediumScreen = !(isLargeScreen || isSmallScreen);
-
   return (
-    <ScreenSizeContext.Provider value={{ isMediumScreen, isSmallScreen }}>
-      <Router>
-        <div
-          className={
-            "App" +
-            (isSmallScreen ? " small-screen" : "") +
-            (isMediumScreen ? " medium-screen" : "") +
-            (isLargeScreen ? " large-screen" : "")
-          }
-        >
-          <header>
-            <NavLink id="logo" to="/">
-              <img src={companyLogo} alt="logo" />
+    <Router>
+      <div className="App">
+        <header>
+          <NavLink id="logo" to="/">
+            <img src={companyLogo} alt="logo" />
+          </NavLink>
+          <nav className="navLinks">
+            <NavLink className="navLink" to="/">
+              Home
             </NavLink>
-            <nav className="navLinks">
-              <NavLink className="navLink" to="/">
-                Home
-              </NavLink>
-              <NavLink className="navLink" to="/about-us">
-                About Us
-              </NavLink>
-              <NavLink className="navLink" to="/contact-us">
-                Contact Us
-              </NavLink>
-              <button className="login-button">Log in</button>
-            </nav>
-          </header>
+            <NavLink className="navLink" to="/about-us">
+              About Us
+            </NavLink>
+            <NavLink className="navLink" to="/contact-us">
+              Contact Us
+            </NavLink>
+            <button className="login-button">Log in</button>
+          </nav>
+        </header>
 
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about-us" element={<AboutUs />} />
-              <Route path="contact-us" element={<ContactUs />} />
-            </Routes>
-          </main>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="contact-us" element={<ContactUs />} />
+          </Routes>
+        </main>
 
-          <footer>
-            <p>
-              Website Devlopment by <a href="www.avamae.co.uk">AVAMAE</a>
-            </p>
-          </footer>
-        </div>
-      </Router>
-    </ScreenSizeContext.Provider>
+        <footer>
+          <p>
+            Website Devlopment by <a href="www.avamae.co.uk">AVAMAE</a>
+          </p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
